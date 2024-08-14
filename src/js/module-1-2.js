da3866275c1d0b7c865f903568d61d620fd6fbaa
Ihor Kharatin <ikharatin44@gmail.com>
2024-08-14 22:34:50 +0200

    module 1

--- src/js/module-1-2.js
+++ src/js/module-1-2.js
@@ -53,25 +53,60 @@
 // Напишіть функцію яка приймає число num. Функція повинна повернути булеве значення true якщо кількість цифр у числі парна. В іншому випадку функція повинна повернути булеве значення false.
 // ```
 
-function task4(num) {
-  const len = num.toString().length;
-  return len % 2 === 0;
-}
+// function task4(num) {
+//   const len = num.toString().length;
+//   return len % 2 === 0;
+// }
+
+//!-------------------------HOMEWORK---------------------
 
 // ```
 // Задача-5
 // Напишіть функцію, яка приймає два параметри num1 та num2. Функція повинна повернути булеве значення true, якщо num1 більше, ніж num2. В іншому випадку функція повинна повернути булеве значення false.
 // ```
 
+// function task5(num1, num2) {
+
+//   if (num1 > num2) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(task5(3, 2));
+// console.log(task5(2, 3));
+
 // ```
 // Задача-6
 // Напишіть функцію, яка приймає два параметри num1 та num2. Функція повинна повернути булеве значення true, якщо num1 ділиться на num2 без остачі. В іншому випадку функція повинна повернути булеве значення false.
 // ```
 
-// ```
+// function task6(num1, num2) {
+//   if (num1 % num2 === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(task6(2, 3));
+// console.log(task6(4, 2));
+
 // Задача-7
 // Напишіть функцію, яка приймає число num. Функція повинна повернути булеве значення true, якщо num є непарним числом. В іншому випадку функція повинна повернути булеве значення false.
 // ```
+
+// function task7(num) {
+//   if (num !== 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(task7(3));
+// console.log(task7(2));
 
 // ```
 // Задача-8
@@ -80,15 +115,48 @@ function task4(num) {
 // Підказка: допоможе toLowerCase();
 // ```
 
+// function task8(str) {
+//   if (str === str.toLowerCase()) {
+
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(task8('hello')); //true;
+// console.log(task8('Hello')); //false
+
 // ```
 // Задача-9
 // Напишіть функцію, яка приймає два параметри str1 та str2. Функція повинна повернути булеве значення true, якщо довжина str1 більше, ніж довжина str2. В іншому випадку функція повинна повернути булеве значення false.
 // ```
 
+// function task9(str1, str2) {
+//   if (str1.length > str2.length) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(task9('9', '8'));  // false
+// console.log(task9('hello', 'h')); // true
+
 // ```
 // Задача-12
 // Напишіть функцію, яка приймає рядок str. Функція повинна повернути булеве значення true, якщо str містить хоча б один пробіл. В іншому випадку функція повинна повернути булеве значення false.
 // ```
+
+// function task12(str) {
+//   if (str.includes(' ')) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// console.log(task12('hello world')); // true;
+// console.log(task12('hello')); //false;
 
 // ```
 // Задача-14
@@ -96,41 +164,125 @@ function task4(num) {
 
 // Підказка: тут допоможе метод масиву reverse();
 // ```
+// function task14(str) {
+//   const str2 = str.split('').reverse().join('');
+//   console.log(str, str2);
+
+//   if (str === str2) {
+//     return true;
+//   } else return false;
+// }
+
+// console.log(task14('alla alla'));
+// console.log(task14('allo'));
 
 // ```
 // Задача-15
 // Напишіть функцію, яка приймає два параметри num1 та num2. Функція повинна повернути булеве значення true, якщо num1 менше або дорівнює num2. В іншому випадку функція повинна повернути булеве значення false.
 // ```
 
+// function task15(num1, num2) {
+//   if (num1 <= num2) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(task15(2, 2));
+// console.log(task15(3, 2));
+
 // ```
 // Задача-17
 // Напишіть функцію, яка приймає рядок str. Функція повинна повернути булеве значення true, якщо перша літера str є голосною (a, e, i, o, u). В іншому випадку функція повинна повернути булеве значення false.
 // ```
+// function task17(str) {
+//   const str1 = str[0];
+//   const str2 = 'aeiou';
+//   if (str2.includes(str1)) {
+//     return true;
+//   } else return false;
+// }
+
+// console.log(task17('hello'));
+// console.log(task17('alla'));
 
 // ```
 // Задача-19
 // Напишіть функцію, яка приймає два параметри num1 та num2. Функція повинна повернути булеве значення true, якщо num1 і num2 рівні. В іншому випадку функція повинна повернути булеве значення false.
 // ```
 
+// function task19(num1, num2) {
+//   if (num1 === num2) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(task19(2, 2));
+// console.log(task19(3, 2));
+
 // ```
 // Задача-20
 // Напишіть функцію, яка приймає число num. Функція повинна повернути булеве значення true, якщо num більше або дорівнює нулю. В іншому випадку функція повинна повернути булеве значення false.
 // ```
+// function task20(num) {
+//   if (num >= 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(task20(20));
+// console.log(task20(-1));
 
 // ```
 // Задача-21
 // Напишіть функцію, яка приймає рядок str. Функція повинна повернути булеве значення true, якщо довжина str є парною. В іншому випадку функція повинна повернути булеве значення false.
 // ```
+// function task21(str) {
+//   const str1 = str.length;
+//   if (str1 % 2 === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(task21('hello'));
+// console.log(task21('hell'));
 
 // ```
 // Задача-22
 // Напишіть функцію, яка приймає два параметри num1 та num2. Функція повинна повернути булеве значення true, якщо num1 ділиться на 2, а num2 не ділиться на 2. В іншому випадку функція повинна повернути булеве значення false.
 // ```
 
+// function task22(num1, num2) {
+//   if (num1 % 2 === 0 && num2 % 2 !== 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(task22(2, 3));
+// console.log(task22(3, 2));
+
 // ```
 // Задача-23
 // Напишіть функцію, яка приймає число num. Функція повинна повернути булеве значення true, якщо num більше 100, але менше 200. В іншому випадку функція повинна повернути булеве значення false.
 // ```
+// function task23(num) {
+//   if (num > 100 && num < 200) {
+//     return true;
+//   }
+//   return false;
+// }
+
+// console.log(task23(50));
+// console.log(task23(150));
 
 // ```
 // Задача-24
@@ -139,34 +291,111 @@ function task4(num) {
 // Підказка: допоможе toLowerCase();
 // ```
 
+// function task24(str) {
+//   const str1 = str.toLowerCase();
+//   if (str.includes(str1)) {
+//     return true;
+//   }
+//   return false;
+// }
+
+// console.log(task24('Hello'));
+// console.log(task24('hello'));
+
 // ```
 // Задача-25
 // Напишіть функцію, яка приймає два параметри str1 та str2. Функція повинна повернути булеве значення true, якщо str1 є частиною str2. В іншому випадку функція повинна повернути булеве значення false.
 // ```
+// function task25(str1, str2) {
+//   if (str2.includes(str1)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(task25('hello', 'world'));
+// console.log(task25('hello', 'hello world'));
 
 // ```
 // Задача-26
 // Напишіть функцію, яка приймає число num. Функція повинна повернути булеве значення true, якщо num є негативним числом. В іншому випадку функція повинна повернути булеве значення false.
 // ```
 
+// function task26(num) {
+//   if (num < 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(task26(2));
+// console.log(task26(-2));
+
 // ```
 // Задача-27
 // Напишіть функцію, яка приймає два параметри num1 та num2. Функція повинна повернути булеве значення true, якщо різниця між num1 та num2 більше 50. В іншому випадку функція повинна повернути булеве значення false.
 // ```
+// function task27(num1, num2) {
+//   if (num1 - num2 > 50) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(task27(70, 50));
+// console.log(task27(700, 500));
 
 // ```
 // Задача-28
 // Напишіть функцію, яка приймає число num. Функція повинна повернути булеве значення true, якщо num є нуль. В іншому випадку функція повинна повернути булеве значення false.
 // ```
 
+// function task28(num) {
+//   if (num === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(task28(0));
+// console.log(task28(1));
+
 // ```
 // Задача-29
 // Напишіть функцію, яка приймає два параметри str1 та str2. Функція повинна повернути булеве значення true, якщо остання літера str1 є голосною (a, e, i, o, u). В іншому випадку функція повинна повернути булеве значення false.
 // ```
+// function task29(str1, str2) {
+//   const str3 = 'aeiou';
+//   const str4 = str1.slice(-1);
+
+//   if (str3.includes(str4)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(task29('hell'));
+// console.log(task29('hello')); //!!!!!!!!!!!!!!!!!!!!!!!!!
 
 // ```
 // Задача-30
 // Напишіть функцію, яка приймає число num. Функція повинна повернути булеве значення true, якщо num є кратним 10. В іншому випадку функція повинна повернути булеве значення false.
+
+// function task30(num) {
+//   if (num % 10 === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(task30(7));
+// console.log(task30(10)); //!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 // ```
 
@@ -187,35 +416,35 @@ function task4(num) {
 // Яким буде результат виразів?
 
 // ```js
-// console.log(5 > 4);
+// console.log(5 > 4); //true
 
-// console.log(10 >= '7');
+// console.log(10 >= '7'); //true
 
-// console.log('2' > '12');
+// console.log('2' > '12'); // true
 
-// console.log('2' < '12');
+// console.log('2' < '12'); //false
 
-// console.log('4' == 4);
+// console.log('4' == 4); //true
 
-// console.log('6' === 6);
+// console.log('6' === 6); // false
 
-// console.log('false' === false);
+// console.log('false' === false); //false
 
-// console.log(1 == true);
+// console.log(1 == true); //true
 
-// console.log(1 === true);
+// console.log(1 === true); //false
 
-// console.log('0' == false);
+// console.log('0' == false); //true
 
-// console.log('0' === false);
+// console.log('0' === false); //false
 
-// console.log('Papaya' < 'papaya');
+// console.log('Papaya' < 'papaya'); //true
 
-// console.log('Papaya' === 'papaya');
+// console.log('Papaya' === 'papaya'); //false
 
-// console.log(undefined == null);
+// console.log(undefined == null); //true
 
-// console.log(undefined === null);
+// console.log(undefined === null); //false
 // ```
 
 // ---
